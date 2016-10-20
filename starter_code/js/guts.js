2c6453f97358cb649eb049c49f91168cc8ee67a3
kevinhui98 <kevin.hui@envirostudies.org>
2016-10-20 22:13:48 +0000

    this is kevin hui calculator

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -1,4 +1,27 @@
 // All the code you will need to create for this project will be completed below
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
-
+    function multiply(a,b){
+       return a*b;
+    }
+    function divide(a,b){
+        return a/b;
+    }
+    function subtract(a,b){
+        return a-b;
+    }
+    function add(a,b){
+        return a+b;
+    }
+    function power(a,b){
+         return Math.pow(a,b);
+    }
+    function square_root(a){
+        return Math.sqrt(a);
+    }
+    function logBase10(a){
+        return Math.log10(a);
+    }
+    function generateRandomNumber(){
+        return Math.floor(Math.random()*10000)+0;
+    }
